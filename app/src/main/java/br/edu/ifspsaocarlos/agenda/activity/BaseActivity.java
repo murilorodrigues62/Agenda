1a00b1f5e5d11eee447733d50638dd8c1d170ac1
Murilo Rodrigues <murilorodrigues62@gmail.com>
2015-12-05 10:13:54 -0200

    Uso de Content Provide e desuso de camada DAO.

---
+++ app/src/main/java/br/edu/ifspsaocarlos/agenda/activity/BaseActivity.java
@@ -0,0 +1,163 @@
+package br.edu.ifspsaocarlos.agenda.activity;
+
+import android.app.SearchManager;
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifspsaocarlos.agenda.R;
+import br.edu.ifspsaocarlos.agenda.adapter.ContatoArrayAdapter;
+import br.edu.ifspsaocarlos.agenda.contentprovider.ContatoProvider;
+import br.edu.ifspsaocarlos.agenda.data.ContatoDAO;
+import br.edu.ifspsaocarlos.agenda.model.Contato;
+
+public class BaseActivity extends AppCompatActivity {
+
+    //protected ContatoDAO cDAO=new ContatoDAO(this);
+    public ListView list;
+    public ContatoArrayAdapter adapter;
+    protected SearchView searchView;
+
+    protected Uri uriContato= ContatoProvider.Contatos.CONTENT_URI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        list = (ListView) findViewById(R.id.listView);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View arg1, int arg2,
+                                    long arg3) {
+                Contato contact = (Contato) adapterView.getAdapter().getItem(arg2);
+                Intent inte = new Intent(getApplicationContext(), DetalheActivity.class);
+                inte.putExtra("contact", contact);
+                startActivityForResult(inte, 0);
+
+            }
+
+        });
+
+
+        registerForContextMenu(list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.pesqContato).getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setIconifiedByDefault(true);
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        // TODO Auto-generated method stub
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater m = getMenuInflater();
+        m.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        ContatoArrayAdapter adapter = (ContatoArrayAdapter)list.getAdapter();
+        Contato contact = adapter.getItem(info.position);
+
+        switch(item.getItemId()){
+
+            case R.id.delete_item:
+                //cDAO.deleteContact(contact);
+                getContentResolver().delete(ContentUris.withAppendedId(uriContato, contact.getId()), null,null);
+                Toast.makeText(getApplicationContext(), "Removido com sucesso", Toast.LENGTH_SHORT).show();
+                buildListView();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+
+    protected void buildListView() {
+        //List<Contato> values = cDAO.buscaTodosContatos();
+
+        List<Contato> values = new ArrayList<Contato>();
+        Cursor cursor = getContentResolver().query(uriContato, null, null, null, null);
+
+        if (cursor!=null)
+            values=cursorTolist(cursor);
+
+        adapter = new ContatoArrayAdapter(this, values);
+        list.setAdapter(adapter);
+
+    }
+
+    protected void buildSearchListView(String query) {
+
+        //List<Contato> values = cDAO.buscaContato(query);
+
+        List<Contato> values = new ArrayList<Contato>();
+
+        String where= ContatoProvider.Contatos.KEY_NOME  + " like ? or " +
+                      ContatoProvider.Contatos.KEY_FONE  + " like ? or " +
+                      ContatoProvider.Contatos.KEY_EMAIL + " like ? ";
+
+        String[] whereargs = new String[] {"%" + query + "%", "%" + query + "%", "%" + query + "%"};
+
+        Cursor  cursor = getContentResolver().query(uriContato,null,where,whereargs,null,null);
+
+        if (cursor!=null)
+            values=cursorTolist(cursor);
+
+        adapter= new ContatoArrayAdapter(this, values);
+        list.setAdapter(adapter);
+
+    }
+
+    public List<Contato> cursorTolist(Cursor cursor)
+    {
+        List<Contato> values=new ArrayList<Contato>();
+        while (cursor.moveToNext()) {
+            Contato c = new Contato();
+            c.setId(cursor.getInt(0));
+            c.setNome(cursor.getString(1));
+            c.setFone(cursor.getString(2));
+            c.setEmail(cursor.getString(3));
+            c.setFone2(cursor.getString(4));
+            c.setNiver(cursor.getString(5));
+            values.add(c);
+        }
+        return values;
+    }
+
+}
---
+++ app/src/main/java/br/edu/ifspsaocarlos/agenda/activity/DetalheActivity.java
@@ -0,0 +1,151 @@
+package br.edu.ifspsaocarlos.agenda.activity;
+
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.edu.ifspsaocarlos.agenda.R;
+//import br.edu.ifspsaocarlos.agenda.data.ContatoDAO;
+import br.edu.ifspsaocarlos.agenda.contentprovider.ContatoProvider;
+import br.edu.ifspsaocarlos.agenda.model.Contato;
+
+public class DetalheActivity extends AppCompatActivity {
+    private Contato c;
+    //private ContatoDAO cDAO;
+
+    Uri uriContato= ContatoProvider.Contatos.CONTENT_URI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getIntent().hasExtra("contact"))
+        {
+
+            this.c = (Contato) getIntent().getSerializableExtra("contact");
+            EditText nameText = (EditText)findViewById(R.id.editText1);
+            nameText.setText(c.getNome());
+
+            EditText foneText = (EditText)findViewById(R.id.editText2);
+            foneText.setText(c.getFone());
+
+            EditText emailText = (EditText)findViewById(R.id.editText3);
+            emailText.setText(c.getEmail());
+
+            EditText txtFone2 = (EditText)findViewById(R.id.edtFone2);
+            txtFone2.setText(c.getFone2());
+
+            EditText txtNiver = (EditText)findViewById(R.id.edtNiver);
+            txtNiver.setText(c.getNiver());
+
+            int pos =c.getNome().indexOf(" ");
+            if (pos==-1)
+                pos=c.getNome().length();
+
+            setTitle(c.getNome().substring(0,pos));
+        }
+
+      //  cDAO = new ContatoDAO(this);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detalhe, menu);
+        if (!getIntent().hasExtra("contact"))
+        {
+            MenuItem item = menu.findItem(R.id.delContato);
+            item.setVisible(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.salvarContato:
+                salvar();
+                return true;
+            case R.id.delContato:
+
+                //cDAO.deleteContact(c);
+                getContentResolver().delete(ContentUris.withAppendedId(uriContato, c.getId()), null,null);
+
+                Toast.makeText(getApplicationContext(), "Removido com sucesso", Toast.LENGTH_SHORT).show();
+                Intent resultIntent = new Intent();
+                setResult(RESULT_OK,resultIntent);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void salvar()
+    {
+        String name = ((EditText) findViewById(R.id.editText1)).getText().toString();
+        String fone = ((EditText) findViewById(R.id.editText2)).getText().toString();
+        String email = ((EditText) findViewById(R.id.editText3)).getText().toString();
+        String fone2 = ((EditText) findViewById(R.id.edtFone2)).getText().toString();
+        String niver = ((EditText) findViewById(R.id.edtNiver)).getText().toString();
+
+        ContentValues valores=new ContentValues();
+        valores.put(ContatoProvider.Contatos.KEY_NOME,  name);
+        valores.put(ContatoProvider.Contatos.KEY_FONE,  fone);
+        valores.put(ContatoProvider.Contatos.KEY_EMAIL, email);
+        valores.put(ContatoProvider.Contatos.KEY_FONE2, fone2);
+        valores.put(ContatoProvider.Contatos.KEY_NIVER, niver);
+
+
+        if (c==null)
+        {
+            c = new Contato();
+            c.setNome(name);
+            c.setFone(fone);
+            c.setEmail(email);
+            c.setFone2(fone2);
+            c.setNiver(niver);
+
+            //cDAO.createContact(c);
+            getContentResolver().insert(uriContato,valores);
+
+            Toast.makeText(this, "Incluído com sucesso", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            c.setNome(name);
+            c.setFone(fone);
+            c.setEmail(email);
+            c.setFone2(fone2);
+            c.setNiver(niver);
+
+            //cDAO.updateContact(c);
+            getContentResolver().update(ContentUris.withAppendedId(uriContato, c.getId()), valores, null, null);
+
+            Toast.makeText(this, "Alterado com sucesso", Toast.LENGTH_SHORT).show();
+        }
+
+        Intent resultIntent = new Intent();
+        setResult(RESULT_OK,resultIntent);
+        finish();
+    }
+
+
+}
